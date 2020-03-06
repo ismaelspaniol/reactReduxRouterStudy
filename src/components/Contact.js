10bf440113135341e12e591137b61126791501a6
ismael spaniol <ismaelspaniol@gmail.com>
2020-03-06 15:02:32 -0300

    inicial

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = (props) => {
+    // setTimeout(() =>{
+    //   props.history.push('/about')  
+    // }, 2000)
+
+    
+    return (
+        <div className='container'>
+            <h4 className='center'>Contact</h4>
+            <p>texto texto textoContact </p>
+        </div>
+    )
+}
+
+export default Contact;
